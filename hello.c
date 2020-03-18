394058dab1a4f6a91d9dcdbb88c76870dfe773d1
Vladimir <iokar911@gmail.com>
2020-03-18 10:06:45 +0300

    add files hello.c hello.h тест гит

---
+++ hello.c
@@ -0,0 +1,18 @@
+//
+//  hello.c
+//  
+//
+//  Created by Vladimir Iokar on 18.03.2020.
+//
+
+#include "hello.h"
+#ifndef hello_h
+#define hello_h
+
+#include <stdio.h>
+
+void main ()
+{
+    printf ("Hello, word\n");
+    return 0;
+}
---
+++ hello.h
@@ -0,0 +1,10 @@
+//
+//  hello.h
+//  
+//
+//  Created by Vladimir Iokar on 18.03.2020.
+//
+
+
+
+#endif /* hello_h */
